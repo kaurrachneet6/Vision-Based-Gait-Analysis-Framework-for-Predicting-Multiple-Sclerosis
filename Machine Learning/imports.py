2bc128fae7d6d95c502f56b957566e6c62ece755
rk4 <rk4@illinois.edu>
2020-09-16 21:40:27 -0500

    Upload New File

---
+++ Machine Learning/imports.py
@@ -0,0 +1,38 @@
+import numpy as np
+import cv2
+import os
+import glob
+import matplotlib.pyplot as plt
+import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
+from IPython.display import display, HTML
+import seaborn as sns
+import copy
+
+import xgboost 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split, KFold, cross_val_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+from sklearn.externals import joblib
+from sklearn.utils import shuffle
+from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import precision_recall_curve
+from inspect import signature
+from scipy import interp
+from pyitlib import discrete_random_variable as drv
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import LinearSVC
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score, roc_auc_score, roc_curve
+import warnings
+from sklearn.neural_network import MLPClassifier
