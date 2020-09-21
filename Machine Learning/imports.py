c9e6fbfb5c3a07f4d7c16ad01f13f8dbe0e525bb
rk4 <rk4@illinois.edu>
2020-09-21 05:59:44 -0500

    Added pytorch

--- Machine Learning/imports.py
+++ Machine Learning/imports.py
@@ -11,16 +11,16 @@ import seaborn as sns
 import copy
 
 import xgboost 
+import sklearn
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split, KFold, cross_val_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
-from sklearn.externals import joblib
 from sklearn.utils import shuffle
 from sklearn.metrics import roc_curve, auc
-from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import precision_recall_curve, plot_confusion_matrix
 from inspect import signature
 from scipy import interp
 from pyitlib import discrete_random_variable as drv
@@ -33,6 +33,11 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GroupKFold
 from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score, roc_auc_score, roc_curve
+from sklearn.metrics import make_scorer
 import warnings
 from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import label_binarize
+
+import torch
